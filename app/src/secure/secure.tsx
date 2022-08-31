4e1017faf903869e4afa1dbb48480b5c05d6112c
Shafique Jamal <shafique.jamal@gmail.com>
2022-08-31 05:50:58 -0400

    Show token in secure area

--- app/src/secure/secure.tsx
+++ app/src/secure/secure.tsx
@@ -1,10 +1,23 @@
 import { useAuth0 } from '@auth0/auth0-react';
+import { useEffect, useState } from "react";
 import React from 'react';
 import { LogoutButton } from '../components/LogoutButton';
 import { Ping } from '../ping/Ping';
 
 export const Secure = () => {
-  const { isLoading, user } = useAuth0();
+  const { isLoading, user, getAccessTokenSilently } = useAuth0();
+  const [accessToken, setAccessToken] = useState('');
+ 
+  useEffect(() => {
+    (async () => {
+      try {
+        const token = await getAccessTokenSilently();
+        setAccessToken(token)
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, [getAccessTokenSilently, accessToken]);
 
   if (isLoading) {
     return <div>Loading</div>;
@@ -14,6 +27,7 @@ export const Secure = () => {
     <div>
       <h1>Secure area!</h1>
       <p>{JSON.stringify(user)}</p>
+      <p>accessToken:{accessToken}</p>
       <Ping />
       <LogoutButton />
     </div>
